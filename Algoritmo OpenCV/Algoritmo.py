24949ca032722bd2c907ca897a3cfd06a5fbc4c2
lucasagcv <lucas.ag.cv@gmail.com>
2017-10-22 21:39:48 -0200

    Ajustes

--- Algoritmo OpenCV/Algoritmo.py
+++ Algoritmo OpenCV/Algoritmo.py
@@ -7,22 +7,6 @@ import cv2
 import os.path
 import numpy as np
 import time
- 
-def _criarListaPositivos(quantidade):
-	print("----------Criando Lista de Positivos")
-	arquivo = open('positivos.dat', 'w')
-	for i in range(1,quantidade+1):
-		arquivo.write("bdsaida/img%d.jpg 1 0 0 100 100\n" %i)
-	print("----------Lista de Positivos criada")
-	arquivo.close()
-
-def _criarListaNegativos(quantidade):
-	print("----------Criando Lista de Negativos")
-	arquivo = open('negativos.dat', 'w')
-	for i in range(1,quantidade+1):
-		arquivo.write("bdnegativos/img%d.jpg\n" %i)
-	print("----------Lista de Negativos criada")
-	arquivo.close()
 
 # utiliza haarcascadefrontalcatface que vem do OPENCV para processar imagem e retornar somente a face do gato
 def _processa(quantidade):
@@ -78,9 +62,6 @@ def _criarVetor(quantidade):
 
 def _treinar(quantidade1, quantidade2):
 	print("----------Iniciando Treinamento")
-	#os.system('opencv_traincascade -data data -vec positivos.vec -bg negativos.dat -numPos %d -numNeg %d -numStages 10 -minHitRate 0.8 -w 20 -h 20' % (quantidade, quantidade/2))
-	#os.system('opencv_traincascade -data data -vec positivos.vec -bg negativos.dat -numStages 20 -minHitRate 0.999 -maxFalseAlarmRate 0.5 -numPos %d -numNeg %d -w 20 -h 20 -mode ALL -precalcValBufSize 1024  -precalcIdxBufSize 1024' % (quantidade, quantidade*0.6))
-	#os.system('opencv_traincascade -data data -vec positivos.vec -bg negativos.dat -numPos %d -numNeg %d-numStages 15 -featureType HAAR -minHitRate 0.999 -maxFalseAlarmRate 0.5 -w 20 -h 20' % (quantidade1, quantidade2))
 	os.system('opencv_traincascade -data data -vec positivos.vec -bg negativos.dat -numPos %d -numNeg %d -numStages 18  -featureType HAAR -minHitRate 0.999 -maxFalseAlarmRate 0.5 -w 20 -h 20' % (quantidade1, quantidade2))
 	print("----------Treinamento concluido")
 
@@ -131,43 +112,12 @@ def _testaFace():
 	cv2.imshow("Face", imagemCinza)
 	cv2.waitKey(0)
 
-
-# testa a imagem teste.jpg na pasta raiz do projeto
-def _testaBanco(quantidade):
-	for contador in range(1,quantidade+1):
-		if os.path.isfile("bdtestes/a (%d).jpg" % contador):
-			# transforma em cinza
-			imagemNormal = cv2.imread("bdtestes/a (%d).jpg" %contador)
-			imagemCinza = cv2.cvtColor(imagemNormal, cv2.COLOR_BGR2GRAY)
-
-			# cria imagem cortada
-			imagemCortada = imagemCinza
-
-			# detectando
-			detector = cv2.CascadeClassifier("haarcascade_gatoSiames.xml")
-			rects = detector.detectMultiScale(imagemCinza, scaleFactor=1.3,
-				minNeighbors=10, minSize=(75, 75))
-
-			# salvar imagem
-			for (i, (x, y, w, h)) in enumerate(rects):
-				cv2.rectangle(imagemCinza, (x, y), (x + w, y + h), (0, 0, 255), 2)
-				imagemCortada = imagemCinza [y:y+h, x:x+w]
-				#cv2.imshow("imagem",imagemCinza)
-				cv2.imwrite("bdresultado/img%d.jpg" % contador,imagemCortada)
-				#cv2.imwrite("bdsalvo/img%d.jpg" % contador,imagemNormal)
-				cv2.waitKey(0)
-
-
 ###chamada de funcoes comeca aqui###
 ## Configuracao de Imagens
 numImgPositivas	= 500
 numImgGerar		= 10
 numImgPosGerada = numImgPositivas * numImgGerar - 1000
 numImgNegativas = numImgPosGerada / 2
-
-## inicio
-#_criarListaPositivos(numImgGerar)
-#_criarListaNegativos(numImgNegativas)
 
 ## algoritmo
 _processa(numImgPositivas)
@@ -179,7 +129,5 @@ _treinar(numImgPosGerada, numImgNegativas)
 ## testes
 #_mostrarVetor()
 #_camera()
-#_testaFace()
-#_testaBanco(100)
 
 ###fim###
